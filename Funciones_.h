916c620dd96379204f8ce84759e106cfb2630d0e
Juan Manuel Alvarez <juanm.alvarezc@udea.edu.co>
2024-10-12 02:24:40 -0500

    Signed-off-by: Juan Manuel Alvarez <juanm.alvarezc@udea.edu.co> copias

--- Funciones_.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCIONES__H
-#define FUNCIONES__H
-#include "ClaseRedNacional.h"
-void serviciosDeGestionDeRed(RedNacional& red);
-void serviciosDeGestionDeEstacionesDeServicio(RedNacional& red);
-//void sistemaDeVerificacionDeFugas();
-//void simulacionDeVentas();
-#endif // FUNCIONES__H
